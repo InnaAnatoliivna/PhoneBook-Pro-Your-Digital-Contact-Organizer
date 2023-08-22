5303f291c315d61bb80c451c3441b76a92a8b8ff
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-08-22 22:03:35 +0200

    Update Swiper.js

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -1,35 +1,35 @@
-import React from 'react'
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
-import { Navigation, Pagination } from 'swiper/modules';
+// import React from 'react'
+// // import Swiper JS
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import 'swiper/css';
+// import { Navigation, Pagination } from 'swiper/modules';
 
-// const swiper = new Swiper(...);
+// // const swiper = new Swiper(...);
 
-const SwiperHomes = () => {
-    return (
-        // < !--Slider main container-- >
-        <div class="swiper">
-            {/* <!-- Additional required wrapper --> */}
-            <div class="swiper-wrapper">
-                <!-- Slides -->
-                <div class="swiper-slide">Slide 1</div>
-                <div class="swiper-slide">Slide 2</div>
-                <div class="swiper-slide">Slide 3</div>
-                <div class="swiper-slide">Slide 3</div>
-            </div>
-            {/* <!-- If we need pagination --> */}
-            <div class="swiper-pagination"></div>
+// const SwiperHomes = () => {
+//     return (
+//         // < !--Slider main container-- >
+//         <div class="swiper">
+//             {/* <!-- Additional required wrapper --> */}
+//             <div class="swiper-wrapper">
+//                 <!-- Slides -->
+//                 <div class="swiper-slide">Slide 1</div>
+//                 <div class="swiper-slide">Slide 2</div>
+//                 <div class="swiper-slide">Slide 3</div>
+//                 <div class="swiper-slide">Slide 3</div>
+//             </div>
+//             {/* <!-- If we need pagination --> */}
+//             <div class="swiper-pagination"></div>
 
-            {/* <!-- If we need navigation buttons --> */}
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
+//             {/* <!-- If we need navigation buttons --> */}
+//             <div class="swiper-button-prev"></div>
+//             <div class="swiper-button-next"></div>
 
-            {/* <!-- If we need scrollbar --> */}
-            <div class="swiper-scrollbar"></div>
-        </div>
-    )
-}
+//             {/* <!-- If we need scrollbar --> */}
+//             <div class="swiper-scrollbar"></div>
+//         </div>
+//     )
+// }
 
-export default Swiper
+// export default Swiper
