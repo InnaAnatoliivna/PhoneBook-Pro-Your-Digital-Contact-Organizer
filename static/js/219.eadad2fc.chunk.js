2b8f31c6651ca667003b7746d5eac8b678e8cac3
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-08-24 14:22:23 +0000

    Deploying to gh-pages from @ InnaAnatoliivna/goit-react-hw-08-phonebook-2@53f1b3ced15ecbdfe89df27444af6c8fc51bffa9 🚀

---
+++ static/js/219.eadad2fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[219],{4219:function(e,t,n){n.r(t);var r=n(2791),u=n(7689),a=n(3329);t.default=function(){var e=(0,u.s0)();return(0,r.useEffect)((function(){var t=setTimeout((function(){e("/")}),4e3);return function(){clearTimeout(t)}}),[e]),(0,a.jsx)("h2",{className:"text-message",children:"404 Not Found. Will be redicted to the home page!"})}}}]);
+//# sourceMappingURL=219.eadad2fc.chunk.js.map
