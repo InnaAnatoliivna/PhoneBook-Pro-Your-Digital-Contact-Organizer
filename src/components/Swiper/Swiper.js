6147fda1363aa41a13e9c3b6ee5dfb59fc4d8fd9
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-08-24 22:54:18 +0200

    +swiper

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -1,43 +1,14 @@
-// import React from 'react'
-// //image
-// import screenPhoneBook1 from 'images/nav-1.jpg'
-// import screenPhoneBook2 from 'images/singup-2.jpg'
-// import screenPhoneBook3 from 'images/login-3.jpg'
-// import screenPhoneBook4 from 'images/user-4.jpg'
-// import screenPhoneBook5 from 'images/contacts-4.jpg'
-// import css from './swiper.module.css'
-
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// // import Swiper core and required modules
-// import { Navigation, Pagination, Scrollbar, A11y, EffectFade } from 'swiper/modules';
-
-// // import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
-// import 'swiper/swiper.min.css';
-// import 'swiper/modules/autoplay/autoplay.min.css';
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
-// // Swiper.use([Navigation, Pagination, Scrollbar, A11y]); //+Autoplay
-// // Styles must use direct files imports
-// import 'swiper/swiper.scss'; // core Swiper
-// import 'swiper/modules/navigation/navigation.scss'; // Navigation module
-// import 'swiper/modules/pagination/pagination.scss'; // Pagination module
-
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, A11y, EffectFade, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import css from './swiper.module.css';
+//image
 import screenPhoneBook1 from 'images/nav-1.jpg';
 import screenPhoneBook2 from 'images/singup-2.jpg';
 import screenPhoneBook3 from 'images/login-3.jpg';
 import screenPhoneBook4 from 'images/user-4.jpg';
 import screenPhoneBook5 from 'images/contacts-4.jpg';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -45,36 +16,116 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const SwiperHomes = () => {
+    const swiperRef = useRef(null); // Створіть посилання useRef
+    useEffect(() => {
+        const swiperInstance = swiperRef.current.swiper;
+        const autoplayInterval = setInterval(() => {
+            if (swiperInstance) {
+                swiperInstance.slideNext();
+            }
+        }, 4000);
 
+        return () => clearInterval(autoplayInterval);
+    }, []);
     return (
         <div className='container'>
             <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
-                autoplay={{ delay: 5000 }}
-                loop={true}
-                spaceBetween={20}
-                slidesPerView={5}
-                navigation
+                ref={swiperRef}
+                modules={[Navigation, Pagination, A11y, EffectFade, Autoplay]}
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={false}
                 pagination={{ clickable: true }}
-                // scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('Зміна слайда')}
+                navigation={true}
+                mousewheel={true}
+                loopedSlides={5}
                 effect="fade"
+                className={css.swiper}
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    1024: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                }}
             >
+
                 <SwiperSlide>
-                    <div className={css.thumb}><img src={screenPhoneBook1} alt='navigation' width="600" height="350" className={css.iconBook} /></div>
+                    <div className={css.thumb}>
+                        <img src={screenPhoneBook1}
+                            alt='navigation'
+                            srcSet={`
+                            ${screenPhoneBook1} 320w,
+                            ${screenPhoneBook1} 768w, 
+                            ${screenPhoneBook1} 1280w`}
+                            sizes="
+                            (max-width: 320px) 320px, 
+                            (max-width: 768px) 768px, 1280px"
+                            className={css.iconBook} />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={css.thumb}><img src={screenPhoneBook2} alt='how to sign up' width="600" height="350" className={css.iconBook} /></div>
+                    <div className={css.thumb}>
+                        <img src={screenPhoneBook2}
+                            alt='how to sign up'
+                            srcSet={`
+                            ${screenPhoneBook2} 320w, 
+                            ${screenPhoneBook2} 768w, 
+                            ${screenPhoneBook2} 1280w`}
+                            sizes="
+                            (max-width: 320px) 320px, 
+                            (max-width: 768px) 768px, 1280px"
+                            className={css.iconBook} />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={css.thumb}><img src={screenPhoneBook3} alt='how to login' width="600" height="350" className={css.iconBook} /></div>
+                    <div className={css.thumb}>
+                        <img src={screenPhoneBook3}
+                            alt='how to login'
+                            srcSet={`
+                            ${screenPhoneBook3} 320w, 
+                            ${screenPhoneBook3} 768w, 
+                            ${screenPhoneBook3} 1280w`}
+                            sizes="
+                            (max-width: 320px) 320px, 
+                            (max-width: 768px) 768px, 1280px"
+                            className={css.iconBook} />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={css.thumb}><img src={screenPhoneBook4} alt='user account' width="600" height="350" className={css.iconBook} /></div>
+                    <div className={css.thumb}>
+                        <img src={screenPhoneBook4}
+                            alt='user account'
+                            srcSet={`
+                            ${screenPhoneBook4} 320w, 
+                            ${screenPhoneBook4} 768w, 
+                            ${screenPhoneBook4} 1280w`}
+                            sizes="
+                            (max-width: 320px) 320px, 
+                            (max-width: 768px) 768px, 1280px"
+                            className={css.iconBook} />
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className={css.thumb}><img src={screenPhoneBook5} alt='your contacts' width="600" height="350" className={css.iconBook} /></div>
+                    <div className={css.thumb}>
+                        <img src={screenPhoneBook5}
+                            alt='your contacts'
+                            srcSet={`
+                            ${screenPhoneBook5} 320w, 
+                            ${screenPhoneBook5} 768w, 
+                            ${screenPhoneBook5} 1280w`}
+                            sizes="
+                            (max-width: 320px) 320px, 
+                            (max-width: 768px) 768px, 1280px"
+                            className={css.iconBook} />
+                    </div>
                 </SwiperSlide>
             </Swiper>
         </div>)
