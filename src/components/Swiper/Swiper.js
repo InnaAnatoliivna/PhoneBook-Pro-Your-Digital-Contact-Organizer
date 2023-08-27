0be6be0c8d51264b0af11755862f249aa0f2e444
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-08-27 18:20:56 +0200

    Update Swiper.js

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -37,7 +37,7 @@ const SwiperHomes = () => {
                 navigation={true}
                 mousewheel={true}
                 loopedSlides={5}
-                effect="fade"
+                // effect="fade"
                 breakpoints={{
                     320: {
                         slidesPerView: 1,
